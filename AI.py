5e81bc91435fd4f8c680763252ba3b12b77ada2d
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2019-12-12 10:18:26 -0700

    Update AI.py

--- AI.py
+++ AI.py
@@ -11,26 +11,26 @@ P1 = AI(Select_name)
 def list():
       print('define words(type define than the word you want to define) \n tell you my name (make sure you type What is your name) \n What i am (type what are you) \n Tell you the time \n'
 
-whatIsYourName = ('my name' + P1)
+whatIsYourName = ('my name' + )
 WhatCanIDO = 'here is a list of things i can do'
 WhatAreYou = 'I am a ANI or Artificial Narrow Intelligence'
 now = datetime.datetime.now()
-currenttime = now.hour + ':', now.min
+currentTime = datetime.time()
 
             
-            
-Maininput = input('Hello what can i do for you')
+while True:            
+      Maininput = input('Hello what can i do for you')
 
 
-if Maininput == 'what is your name':
-      print(whatIsYourName)
-            
-if Maininput == 'what are you:
-            print(WHatCanIDo)
-            
-if Maininput == 'Hey' + Select_name:
-            print('hello' + whatIsYourName+ ',What can i help you with)
+      if Maininput == 'what is your name':
+            print(whatIsYourName)
 
-if Maininput == 'what time is it':
-                  print(currenttime)
+      if Maininput == 'what are you':
+                  print(WHatCanIDo)
+
+      if Maininput == 'Hey jed what can i help you with today' :
+                  print('hello' + whatIsYourName+ ',What can i help you with')
+
+      if Maininput == 'what time is it':
+                        print(currentTime)
                   
