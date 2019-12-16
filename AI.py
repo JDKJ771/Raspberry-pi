675dd46f72b3e5fd77968b4da17dfaba32cbcd30
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2019-12-16 13:27:25 -0700

    Update AI.py

--- AI.py
+++ AI.py
@@ -1,20 +1,22 @@
+import calender
 import datetime
 class AI:
           
   def __init__(self, name):
-            self.name = cyana
-            
-Def calculator():
+            self.name = Jarvis
+ 
+
+Def classic_calculator():
       Num1 = input(‘put your first number here’)
       Num2 = input(‘put your second number here’)
       Add = int(Num1) + int(Num2)
-      Print (Add)
+      Print(Add)
       Mult = int(Num1) * int(Num2)
-      Print (Mult)
+      Print(Mult)
       Divide = int(Num1) / int(Num2)
-      Print (Divide)
+      Print(Divide)
       Sub = int(Num1) - int(Num2)
-      Print (Sub)
+      Print(Sub)
 
 def list():
       ('define words(type define than the word you want to define) \n tell you my name (make sure you type What is your name) \n What i am (type what are you) \n Tell you the time \n')
@@ -24,8 +26,8 @@ WhatCanIDO = 'here is a list of things i can do'
 WhatAreYou = 'I am a ANI or Artificial Narrow Intelligence'
 now = datetime.datetime.now()
 currentTime = datetime.time()
-
-            
+calender = now.date
+input('Welcome My name is Jarvis what can i do for you today')            
 while True:            
       Maininput = input('Hello what can i do for you')
 
@@ -36,11 +38,14 @@ while True:
       if Maininput == 'what are you':
                   print(WHatCanIDo)
 
-      if Maininput == 'Hey jed what can i help you with today' :
-                  print('hello' + whatIsYourName+ ',What can i help you with')
+      if Maininput == 'Hey Jarvis' :
+                  print (input('Yes'))
 
       if Maininput == 'what time is it':
                         print(currentTime)
+      if Maininput == 'whats todays date':
+                    print(calender)
+                              
     
 
          
