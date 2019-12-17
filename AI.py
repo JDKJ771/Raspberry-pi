1f884bb4681ddd5ebdd8487b02f06b012d74372c
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2019-12-17 10:04:53 -0700

    Update AI.py

--- AI.py
+++ AI.py
@@ -1,11 +1,10 @@
-import calender
 import datetime
 
  
 
-Def classic_calculator():
-      Num1 = input(‘put your first number here’)
-      Num2 = input(‘put your second number here’)
+def classic_calculator():
+      Num1 = input('put your first number here')
+      Num2 = input('put your second number here')
       Add = int(Num1) + int(Num2)
       Print(Add)
       Mult = int(Num1) * int(Num2)
@@ -18,7 +17,7 @@ Def classic_calculator():
 def list():
       ('define words(type define than the word you want to define) \n tell you my name (make sure you type What is your name) \n What i am (type what are you) \n Tell you the time \n')
 
-whatIsYourName = ('my name is Jarvis' )
+whatIsYourName = ('my name is Jarvis')
 WhatCanIDO = 'here is a list of things i can do'
 WhatAreYou = 'I am a ANI or Artificial Narrow Intelligence'
 now = datetime.datetime.now()
