424d7a33bbbe1b52a6a286c92c4183dcf1d48ebb
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2019-12-18 13:40:36 -0700

    Update AI.py

--- AI.py
+++ AI.py
@@ -1,29 +1,30 @@
 import datetime
 
  
-
+while True:
+   import random
+   Jarvis_Random = input(random.choice(['hi how can i help','whats up', 'yes']))
 def classic_calculator():
       Num1 = input('what is your fist number')
       Num2 = input('what is your second number')
       Add = int(Num1) + int(Num2)
-      Print(Add)
+      print(Add)
       Mult = int(Num1) * int(Num2)
-      Print(Mult)
+      print(Mult)
       Divide = int(Num1) / int(Num2)
-      Print(Divide)
+      print(Divide)
       Sub = int(Num1) - int(Num2)
       Print(Sub)
 
 def list():
       ('define words(type define than the word you want to define) \n tell you my name (make sure you type What is your name) \n What i am (type what are you) \n Tell you the time \n')
 
-Hey_Jarvis = input('yes')
 whatIsYourName = ('my name is Jarvis')
 WhatCanIDO = 'here is a list of things i can do'
 WhatAreYou = 'I am a ANI or Artificial Narrow Intelligence'
-now = datetime.datetime.now()
-currentTime = datetime.time()
-calender = datetime.date.today()
+cdt = datetime.datetime.now()
+currentTime = cdt.strftime("%H:%M")
+date = datetime.date.today()
 Welcome = input('Welcome my name is Jarvis how can i help')           
 while True:            
       Maininput = input()
@@ -32,12 +33,18 @@ while True:
       if Maininput == 'open calculator':
                print(classic_calculator)
    
-      if Hey_Jarvis == 'open calculator':
+      if Jarvis_Random == 'open calculator':
               print (classic_calculator)
-      if Hey_Jarvis == 'what is your name':
+      if Jarvis_Random == 'what is your name':
               print(whatIsYourName)
-      if Hey_Jarvis == 'can you do me a favor'
+      if Jarvis_Random == 'can you do me a favor'
               print('whats up')
+      if Jarvis_Random == 'whats the date'
+              print('the date is' + date)
+      if Jarvis_Random == 'what time is it'
+              print('The time is' + currentTime)
+      if Jarvis_Random == 'can you do me a favor'
+              print(')
    
     
       if Maininput == 'what is your name':
@@ -47,12 +54,12 @@ while True:
                   print(WhatCanIDo)
 
       if Maininput == 'Hey Jarvis' :
-                  print (Hey_Jarvis)
+                  print (Jarvis_Random)
 
       if Maininput == 'what time is it':
-                        print(currentTime)
+                   print(currentTime)
       if Maininput == 'whats todays date':
-                    print(calender)
+                    print(date)
                               
     
 
