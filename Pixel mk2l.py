b8d17281ddac4be0b1ee27c7df4a10fcc5149da0
JDKJ771 <55402460+JDKJ771@users.noreply.github.com>
2020-09-30 13:38:25 -0600

    Update and rename Pixel.py to Pixel mk2l.py

---
+++ Pixel mk2l.py
@@ -0,0 +1,27 @@
+import datetime
+import random
+from time import sleep
+#import pygame # for voice services
+#import machine #for controlling prossesor and microphone and speaker
+
+cd = datetime.datetime.now()
+ct = cd.strftime("%H:%M)
+
+#intro for commandline
+name = input('What would you like me to call you: ')
+sirmiss = input("would you like me to call you sir or miss (you can change this by the way) ")
+
+intro = ['Hello' + name, 'Hi' + sirmiss, 'Hello' + sirmiss, 'Hi' + name] 
+
+
+while True:
+    Termanal = input(random.choice(intro)).strip().lower()
+    
+    
+    if Termanal == "hello" or Termanal == "hi" or Termanal == "whats up":
+        random.choice(intro)
+        
+    elif Termanal == "time" or Termanal == "What time is it" or Termanal == "clock":
+        print(ct)
+                 
+    elif Termanal == ""
